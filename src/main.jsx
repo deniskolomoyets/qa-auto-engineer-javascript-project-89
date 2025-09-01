86c6d0d97cb2e445d092301c8929ec0db7e0d6b6
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2025-09-01 16:12:29 +0300

    git commit -m "step-5: mount <App/> and embed chatbot inside App (fix main.jsx imports)"

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
-import '@hexlet/chatbot-v2/styles';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Widget from '@hexlet/chatbot-v2';
-// import steps from '@hexlet/chatbot-v2/example-steps';
+import App from './App.jsx';
 import '@hexlet/chatbot-v2/styles';
-import steps from '../__fixtures__/basicSteps.js';
 
-
-const container = document.getElementById('root');
-ReactDOM.createRoot(container)
-  .render(Widget(steps));
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
