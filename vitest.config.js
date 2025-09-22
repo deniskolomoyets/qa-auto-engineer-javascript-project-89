e3fad38e4a1bfe04338967c1a37c88c6214246f8
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2025-09-22 10:36:22 +0300

    fix

---
+++ vitest.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    css: true,
+    globals: true,
+    setupFiles: ['vitest.setup.js'],
+    server: {
+      deps: {
+        inline: [/@hexlet\/.*/],
+      },
+    },
+    deps: {
+      web: {
+        transformCss: true,
+      },
+    },
+  },
+  plugins: [react()],
+});
+
+
