eee1008548646b8496b385ef8d677c7c75f1a0c5
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2025-09-22 12:00:04 +0300

    chore: align with reference config; use @hexlet/chatbot-v2 imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Widget from '@hexlet/chatbot-v2';
-import { validSteps as steps } from '../__fixtures__/steps.js';
+import steps from '@hexlet/chatbot-v2/example-steps';
 
 const App = () => {
   const [form, setForm] = useState({
